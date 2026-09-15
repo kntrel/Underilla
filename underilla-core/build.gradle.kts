eac2175acf27b4ce2348e42fe6e10c3751ca5a82
kntrel <me@kntrel.com>
2026-09-15 01:51:08 -0600

    Migrated KntNbt dependency

--- underilla-core/build.gradle.kts
+++ underilla-core/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 description = "Platform-neutral world reading and terrain merging for Underilla."
 
 dependencies {
-    api("com.github.HydrolienF:KntNBT:2.2.2")
+    api("com.github.kntrel:KntNBT:e93a4e31d4")
     compileOnly("org.slf4j:slf4j-api:2.0.16")
 
     testImplementation(platform("org.junit:junit-bom:6.0.1"))
